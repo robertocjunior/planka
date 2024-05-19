0e7344f633b06f10b40ec430f5b444a6ab0c4c2c
Roberto Casali Júnior <robertocasalijunior@gmail.com>
2024-05-19 02:58:33 -0300

    Revert "test fields 5"
    This reverts commit 762bbcfe319579c35ea271e6551385d99b204d3e.

--- client/src/components/CardModal/DescriptionEdit.jsx
+++ client/src/components/CardModal/DescriptionEdit.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useImperativeHandle, useMemo, useState } from 'react';
+import React, { useCallback, useImperativeHandle, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { Button, Form, Input } from 'semantic-ui-react';
@@ -33,6 +33,7 @@ const DescriptionEdit = React.forwardRef(({ children, defaultValue, onUpdate }, 
     }
 
     setIsOpened(false);
+    setFields(DEFAULT_FIELDS);
   }, [defaultValue, onUpdate, values]);
 
   useImperativeHandle(
@@ -61,18 +62,16 @@ const DescriptionEdit = React.forwardRef(({ children, defaultValue, onUpdate }, 
     close();
   }, [close]);
 
-  const renderInputFields = useMemo(() => {
-    return Object.entries(fields).map(([key, value]) => (
-      <Form.Field
-        key={key}
-        control={Input}
-        name={key}
-        label={value}
-        value={values[key] || ''}
-        onChange={handleFieldChange}
-      />
-    ));
-  }, [fields, values, handleFieldChange]);
+  const renderInputFields = Object.entries(fields).map(([key, value]) => (
+    <Form.Field
+      key={key}
+      control={Input}
+      name={key}
+      label={value}
+      value={values[key] || ''}
+      onChange={handleFieldChange}
+    />
+  ));
 
   if (!isOpened) {
     return React.cloneElement(children, {
